8670f61a0f09efee16bcf680e566ecc4dbe4e647
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-05-11 10:37:34 +0300

    post

--- js/components/Post.js
+++ js/components/Post.js
@@ -1,14 +1,32 @@
-class Post{
-    // parent tevinis el kuriame talpinsim sugeneruota posta
-    // props yra posto savybes 
-    constructor(parent, props) {
-        this.parent = parent;
-        this.props = props;
+class Post {
+  // parent tevinis el kuriame talpinsim sugeneruota posta
+  // props yra posto savybes
+  constructor(parent, props) {
+    this.parent = parent;
+    this.props = props;
 
-        // Atvaizdavimo reikalai 
-        this.element = document.createElement('article');
-    }
-    componentDidMount() {}
+    // Atvaizdavimo reikalai
+    this.element = document.createElement("article");
+    // automatiskai vygdomi metodai po new Post()
+    this.render();
+    this.componentDidMount();
+  }
+  componentDidMount() {
+    // atliekami galutinaii elemto veiksmai
+    this.element.className = "card w-100 h-100 mb-4";
+    // talpinam el DOM
+    this.parent.appendChild(this.element);
+  }
 
-    render()
+  render() {
+    this.element.innerHTML = null;
+    this.element.innerHTML = `
+        <img src="https://placeimg.com/640/480/any" class="card-img-top" alt="dalykas">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+        `;
+  }
 }
